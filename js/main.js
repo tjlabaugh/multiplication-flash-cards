1a4561532904a998c305c6cc62b72480acabb3c6
TJ LaBaugh <tjlabaugh@gmail.com>
2016-05-18 22:22:15 -0400

    Wrote JavaScript functionality

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var problem 	= document.querySelector('.problem');
+var genBttn 	= document.querySelector('#gen-problem');
+var submitBttn	= document.querySelector('#submit');
+var userInput	= document.querySelector('.userInput');
+var message		= document.querySelector('.message');
+var overlay		= document.querySelector('.overlay');
+var randNum1 	= 0;
+var randNum2 	= 0;
+
+// function will generate multiplication problem for user to solve.
+function generate() {
+	randNum1 = Math.floor(Math.random() * 10 + 1);
+	randNum2 = Math.floor(Math.random() * 10 + 1);
+
+	problem.textContent = randNum1 + " x " + randNum2;
+	userInput.focus();
+	overlay.style.display = 'none';
+};
+
+// function will evaluate user's answer.
+function evaluate() {
+	var output;
+
+	if (userInput.value == (randNum1 * randNum2)) {
+		output = "Correct!";
+	}
+	else {
+		output = "Wrong!";
+	}
+
+	output += "<br />" + randNum1 + " x " + randNum2 + " = " + (randNum1 * randNum2);
+	message.innerHTML = output;
+	userInput.value = '';
+	userInput.blur();
+	overlay.style.display = 'block';
+	genBttn.focus();
+};
+
+// Event listeners if the user clicks 'Generate Problem' or hits enter
+genBttn.addEventListener('click', function(e) {
+	generate();
+}, false);
+
+genBttn.addEventListener('keypress', function(e) {
+	if (e.keyCode === 13) {
+		generate();
+	}
+}, false);
+
+// Event listeners if the user clicks 'submit' button or hits enter while in the input box.
+submitBttn.addEventListener('click', function(e) {
+	evaluate();
+}, false);
+
+userInput.addEventListener('keypress', function(e) {
+	if (e.keyCode === 13) {
+		evaluate();
+	}
+}, false);
+
