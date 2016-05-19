adec2ef2cda3e3182ff9fafb8f61e92903face1e
TJ LaBaugh <tjlabaugh@gmail.com>
2016-05-19 15:55:03 -0400

    Added comments to js file

--- js/main.js
+++ js/main.js
@@ -16,8 +16,14 @@ function generate() {
 	randNum2 = Math.floor(Math.random() * 10 + 1);
 
 	problem.textContent = randNum1 + " x " + randNum2;
+
+	// Puts focus on text input so user doesn't have to click to focus each time
 	userInput.focus();
+
+	// Hides overlay message
 	overlay.style.display = 'none';
+
+	// Addes the animation classes to give card flipping over effect
 	var notecardClassVal = notecard.className;
 	notecardClassVal += ' flip';
 	notecard.className = notecardClassVal;
@@ -30,23 +36,32 @@ function generate() {
 function evaluate() {
 	var output;
 
+	// If user answers correctly message "correct" will appear and streak counter will increase 
 	if (userInput.value == (randNum1 * randNum2)) {
 		output = "Correct!";
 		message.style.color = "green";
 		streakCount++;
 	}
+	// If user answers wrong message "wrong" will appear and streak counter will reset
 	else {
 		output = "Wrong!";
 		message.style.color = "red";
 		streakCount = 0;
 	}
 
+	// Adds the problem, correct answer, and streak counter to message
 	output += "<br />" + randNum1 + " x " + randNum2 + " = " + (randNum1 * randNum2);
 	streak.textContent = "Your Streak: " + streakCount;
 	message.innerHTML = output;
+
+	// Resets input text box
 	userInput.value = '';
+
+	// Resets notecard and problem classes
 	notecard.className = 'notecard';
 	problem.className = 'problem';
+
+	// Takes focus off input box, displays overlay, and sets focus to "generate problem button"
 	userInput.blur();
 	overlay.style.display = 'block';
 	genBttn.focus();
